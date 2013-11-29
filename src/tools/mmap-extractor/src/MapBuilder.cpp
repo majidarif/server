14729c81867f24513177a3165e1b2de2a4ab2122
Daniel S. Reichenbach <daniel@kogitoapp.com>
2013-11-29 02:40:05 +0200

    SHA-1: b95041c09bb5334a18d5337f5c9dd96ecddcc77d
    * CMake build system fixes for external ACE usage, and proper use of recastnavigation.
    
    The ACE framework is now an external reference and no longer part of the sources.
    
    You will have to grab a copy of ACE from http://download.dre.vanderbilt.edu/
    and build and install it if you are using Windows.
    
    Linux and BSD users can use available packages from their distributions.
    
    We recommend to use ACE version 6.0.x. Newer versions are currently not
    supported.

--- src/tools/mmap-extractor/src/MapBuilder.cpp
+++ src/tools/mmap-extractor/src/MapBuilder.cpp
@@ -16,14 +16,14 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <DetourNavMeshBuilder.h>
+#include <DetourCommon.h>
+
 #include "MMapCommon.h"
 #include "MapBuilder.h"
 
 #include "MapTree.h"
 #include "ModelInstance.h"
-
-#include "DetourNavMeshBuilder.h"
-#include "DetourCommon.h"
 
 using namespace VMAP;
 
